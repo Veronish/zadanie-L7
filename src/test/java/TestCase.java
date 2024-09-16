fc67ff3fa6faadfb4ab17215e39e94c0637571ff
Veronika <pvpyu@inbox.ru>
2024-09-16 11:43:25 +0300

    2

--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -34,19 +34,19 @@ public class TestCase {
     void test4(int a) {
         switch (a) {
             case 1:
-                assertEquals(Factorial(a), 1, () -> "Факториал 1го неверный");
+                assertEquals(Factorial(a), 1, "Факториал 1го неверный");
                 break;
             case 2:
-                assertEquals(Factorial(a), 2, () -> "Факториал 2x неверный");
+                assertEquals(Factorial(a), 2, "Факториал 2x неверный");
                 break;
             case 3:
-                assertEquals(Factorial(a), 6, () -> "Факториал 3x неверный");
+                assertEquals(Factorial(a), 6, "Факториал 3x неверный");
                 break;
             case 4:
-                assertEquals(Factorial(a), 24, () -> "Факториал 4x неверный");
+                assertEquals(Factorial(a), 24, "Факториал 4x неверный");
                 break;
             case 5:
-                assertEquals(Factorial(a), 120, () -> "Факториал 5и неверный");
+                assertEquals(Factorial(a), 120, "Факториал 5и неверный");
                 break;
             case 18:
                 assertEquals(Factorial(a), 479001600, () -> "Факториал 12и неверный");
@@ -58,16 +58,12 @@ public class TestCase {
     @Test
     @DisplayName("Проверка ввода отрицательных чисел")
     void test5() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Factorial(-1);
-        });
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Factorial(-1));
     }
     @Test
     @DisplayName("Проверка ввода слишком большого числа")
     void test6() {
-        Assertions.assertThrows(OutOfMemoryError.class, () -> {
-            Factorial(21);
-        });
+        Assertions.assertThrows(OutOfMemoryError.class, () -> Factorial(21));
     }
 
 
