e630ab2efd714f6befbe30bb67240d816c51b8f5
Veronika <pvpyu@inbox.ru>
2024-09-16 11:41:19 +0300

    2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,19 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Factorial(4));
+        System.out.println(Factorial(2));
     }
 
-    public static int Factorial(int factorial) {
-        int j = factorial;
+    public static long Factorial(long factorial) {
+
+        if (factorial < 0) {
+            throw new IllegalArgumentException("Число не может быть отрицательным");
+        }
+        if (factorial > 20) {
+            throw new OutOfMemoryError("Невозможно вычисличть факториал больше 20");
+        }
+
+        long j = factorial;
         factorial = 1;
         for (int i = 1; i <= j; i++) {
             factorial = factorial * i;
--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,9 +17,9 @@ public class TestCase {
     }
 
     @Test
-    @DisplayName("Факториал 4")
+    @DisplayName("Факториал 20")
     void test2() {
-        assertEquals(Factorial(4), 24);
+        assertEquals(Factorial(20), 2432902008176640000L);
     }
 
     @Test
@@ -27,12 +28,48 @@ public class TestCase {
         assumeTrue(Factorial(0) == 1);
     }
 
-
+    @DisplayName("Проверка вычислений 1-5 и 12")
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 3, 4, 5})
+    @ValueSource(ints = {1, 2, 3, 4, 5, 12})
     void test4(int a) {
-        assertEquals(Factorial(a), 120, () -> "Факториал не = 5");
+        switch (a) {
+            case 1:
+                assertEquals(Factorial(a), 1, () -> "Факториал 1го неверный");
+                break;
+            case 2:
+                assertEquals(Factorial(a), 2, () -> "Факториал 2x неверный");
+                break;
+            case 3:
+                assertEquals(Factorial(a), 6, () -> "Факториал 3x неверный");
+                break;
+            case 4:
+                assertEquals(Factorial(a), 24, () -> "Факториал 4x неверный");
+                break;
+            case 5:
+                assertEquals(Factorial(a), 120, () -> "Факториал 5и неверный");
+                break;
+            case 18:
+                assertEquals(Factorial(a), 479001600, () -> "Факториал 12и неверный");
+                break;
+        }
+        //assertEquals(Factorial(a), 120, () -> "Факториал не = 5");
     }
+
+    @Test
+    @DisplayName("Проверка ввода отрицательных чисел")
+    void test5() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Factorial(-1);
+        });
+    }
+    @Test
+    @DisplayName("Проверка ввода слишком большого числа")
+    void test6() {
+        Assertions.assertThrows(OutOfMemoryError.class, () -> {
+            Factorial(21);
+        });
+    }
+
 
 
 }
